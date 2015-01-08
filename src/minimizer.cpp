d6600be89929a047fb9834a5481b90a3d3d393d4
Marko Deak <marko.deak@fer.hr>
2015-01-08 00:01:33 +0000

    minimizer: add the outer minimizers into the result

--- src/minimizer.cpp
+++ src/minimizer.cpp
@@ -47,9 +47,21 @@ namespace OLC
 		return minimizers;
 	}
 
+	std::vector<std::vector<Nucleotide>> findOuterMinimizers(const std::vector<Nucleotide> &vector, int k) {
+		std::vector<std::vector<Nucleotide>> minimizers;
+
+		minimizers.push_back(slice(vector, 0, k));
+		minimizers.push_back(slice(vector, vector.size() - k, vector.size()));
+	
+		return minimizers;
+	}
+
 
 	std::vector<std::vector<Nucleotide>> minimize(std::vector<Nucleotide> nucleotideVector, int w, int k) { 
 		std::vector<std::vector<Nucleotide>> innerMinimizers = findInnerMinimizers(nucleotideVector, w, k);
+		std::vector<std::vector<Nucleotide>> outerMinimizers = findOuterMinimizers(nucleotideVector, k);
+
+		std::cout << "INNER MINIMIZERS:" << std::endl;
 		for (int i = 0; i < innerMinimizers.size(); i++) {
 			std::vector<Nucleotide> minimizer = innerMinimizers[i];
 			for (int j = 0; j < minimizer.size(); j++) {
@@ -57,6 +69,16 @@ namespace OLC
 			}
 			std::cout << std::endl;
 		}
+
+		std::cout << "OUTER MINIMIZERS:" << std::endl;
+		for (int i = 0; i < outerMinimizers.size(); i++) {
+			std::vector<Nucleotide> minimizer = outerMinimizers[i];
+			for (int j = 0; j < minimizer.size(); j++) {
+				std::cout << minimizer[j].getNucleotide() << " ";
+			}
+			std::cout << std::endl;
+		}
+
 		return innerMinimizers;
 	}
 
