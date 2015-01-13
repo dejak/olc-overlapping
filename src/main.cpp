03e4f03eef775e9af9542e3daa61161b0e9fc740
Josko <josko.nikolic1@gmail.com>
2015-01-13 17:04:58 +0100

    main: do the simple case

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,16 @@
 #include "result.hpp"
 
 // mutex for shared resources
-static std::mutex g_tasks_mutex;
+static std::mutex g_resource_mutex;
+
+// mutex for result vector
+static std::mutex g_result_mutex;
 
 // conditonal variable for shared resources
-static std::condition_variable g_cv;
+static std::condition_variable g_resource_cv;
+
+// conditonal variable for result vector
+static std::condition_variable g_result_cv;
 
 // global queue with two pointers to the sequences
 // they are compared directly if the both sequence lengths are small enough
@@ -24,60 +30,72 @@ static std::queue<std::tuple<OLC::Sequence*, OLC::Sequence*>> g_sequence_pairs;
 // we use this instead if the sequences are too long
 static std::queue<std::tuple<std::vector<OLC::Minimizer>*, std::vector<OLC::Minimizer>*>> g_minimizer_pairs;
 
+static std::vector<OLC::Result> g_results;
+
 static void worker()
 {
-  // Get the task and remove it from the pool
-  g_tasks_mutex.lock();
-
-  const std::tuple<OLC::Sequence*, OLC::Sequence*> sequence_pair = g_sequence_pairs.front();
-  g_sequence_pairs.pop();
-  const std::tuple<std::vector<OLC::Minimizer>*, std::vector<OLC::Minimizer>*> minimizer_pair = g_minimizer_pairs.front();
-  g_minimizer_pairs.pop();
-
-  g_tasks_mutex.unlock();
-
-  // Get the task sequences
-  const OLC::Sequence* sequence1 = std::get<0>(sequence_pair);
-  const OLC::Sequence* sequence2 = std::get<1>(sequence_pair);
-
-  // Pull out the wrapped nucleotide vectors
-  const std::vector<OLC::Nucleotide> nucleotides1 = sequence1->getNucleotides()->getSequence();
-  const std::vector<OLC::Nucleotide> nucleotides2 = sequence2->getNucleotides()->getSequence();
-
-  // If small enough, no need to use minimizers
-  if (nucleotides1.size() < 20000 && nucleotides2.size() < 20000)
+  while(true)
   {
-    const OLC::Overlap overlap = compare(nucleotides1, nucleotides2);
-    const uint32_t overlapFirstEnd = overlap.getEndFirst();
-    const uint32_t overlapSecondEnd = overlap.getEndSecond();
-    const uint32_t overlapFirstStart = overlap.getStartFirst();
-    const uint32_t overlapSecondStart = overlap.getStartSecond();
-    const uint32_t overlapLength = overlapFirstEnd - overlapFirstStart + 1;
+    // Get the task and remove it from the pool
+    std::unique_lock<std::mutex> resource_lock(g_resource_mutex);
+    g_resource_cv.wait(resource_lock);
 
-    const std::string sequence1_ident = sequence1->getIdentifier();
-    const std::string sequence2_ident = sequence2->getIdentifier();
+    if (g_sequence_pairs.empty())
+      return;
 
-    int32_t ahang = overlapFirstStart;
-    int32_t bhang = nucleotides2.size() - overlapSecondEnd;
+    const std::tuple<OLC::Sequence*, OLC::Sequence*> sequence_pair = g_sequence_pairs.front();
+    g_sequence_pairs.pop();
+    const std::tuple<std::vector<OLC::Minimizer>*, std::vector<OLC::Minimizer>*> minimizer_pair = g_minimizer_pairs.front();
+    g_minimizer_pairs.pop();
 
-    if (overlapSecondStart > overlapFirstStart) {
-      ahang *= -1;
+    resource_lock.unlock();
+    g_resource_cv.notify_one();
+
+    // Get the task sequences
+    const OLC::Sequence* sequence1 = std::get<0>(sequence_pair);
+    const OLC::Sequence* sequence2 = std::get<1>(sequence_pair);
+
+    // Pull out the wrapped nucleotide vectors
+    const std::vector<OLC::Nucleotide> nucleotides1 = sequence1->getNucleotides()->getSequence();
+    const std::vector<OLC::Nucleotide> nucleotides2 = sequence2->getNucleotides()->getSequence();
+
+    // If small enough, no need to use minimizers
+    if (nucleotides1.size() < 20000 && nucleotides2.size() < 20000)
+    {
+      const OLC::Overlap overlap = compare(nucleotides1, nucleotides2);
+      const uint32_t overlapFirstEnd = overlap.getEndFirst();
+      const uint32_t overlapSecondEnd = overlap.getEndSecond();
+      const uint32_t overlapFirstStart = overlap.getStartFirst();
+      const uint32_t overlapSecondStart = overlap.getStartSecond();
+      const uint32_t overlapLength = overlapFirstEnd - overlapFirstStart + 1;
+
+      const std::string sequence1_ident = sequence1->getIdentifier();
+      const std::string sequence2_ident = sequence2->getIdentifier();
+
+      int32_t ahang = overlapFirstStart;
+      int32_t bhang = nucleotides2.size() - overlapSecondEnd;
+
+      if (overlapSecondStart > overlapFirstStart)
+        ahang *= -1;
+
+      if (nucleotides1.size() > overlapSecondEnd)
+        bhang *= -1;
+
+      const OLC::Result result(sequence1_ident, sequence2_ident, overlapLength, ahang, bhang);
+
+      std::unique_lock<std::mutex> result_lock(g_result_mutex);
+      g_result_cv.wait(result_lock);
+
+      g_results.push_back(result);
+
+      result_lock.unlock();
+      g_result_cv.notify_one();
     }
+    else
+    {
 
-    if (nucleotides1.size() > overlapSecondEnd) {
-      bhang *= -1;
     }
-
-    //TODO: Add into result queue
-    const OLC::Result result(sequence1_ident, sequence2_ident, overlapLength, ahang, bhang);
   }
-  else
-  {
-
-  }
-
-
-  return;
 }
 
 int main(int argc, char** argv)
