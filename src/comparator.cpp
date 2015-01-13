fb426367b56f718684fddfc07dbfd64b171cf99e
Josko <josko.nikolic1@gmail.com>
2015-01-13 06:38:41 +0100

    comparator: formatting

--- src/comparator.cpp
+++ src/comparator.cpp
@@ -1,93 +1,110 @@
 #include "comparator.hpp"
 
-#include <iostream>
-
 namespace OLC
 {
-Position::Position(const int i, const int j) 
-	:i_(i), j_(j)
-{}
 
-int Position::get(int index) const {
-	if (index == 0) {
-		return i_;
-	} else {
-		return j_;
-	}
+Position::Position(const int i, const int j)
+  : i_(i),
+    j_(j)
+{
+
 }
 
-std::vector<Position> compare(std::vector<int> first, std::vector<int> second) {
-	std::vector<std::vector<int>> values;
-	std::vector<std::vector<Position>> positions;
+int Position::get(int index) const
+{
+  if (index == 0)
+    return i_;
+  else
+    return j_;
+}
 
-	for (std::size_t i = 0; i < first.size() + 1; i++) {
-		std::vector<int> row;
-		std::vector<Position> positionRow;
-		for (std::size_t j = 0; j < second.size() + 1; j++) {
-			row.push_back(0);
-			Position pos(0, 0);
-			positionRow.push_back(pos);
-		}
-		values.push_back(row);
-		positions.push_back(positionRow);
-	}
+std::vector<Position> compare(const std::vector<int>& first, const std::vector<int>& second)
+{
+  std::vector<std::vector<int>> values;
+  std::vector<std::vector<Position>> positions;
 
-	int maxValue = 0;
-	Position maxPosition(0, 0);
+  for (std::size_t i = 0; i < first.size() + 1; ++i)
+  {
+    std::vector<int> row;
+    std::vector<Position> positionRow;
 
-	for (std::size_t  i = 1; i < first.size() + 1; i++) {
-		for (std::size_t  j = 1; j < first.size() + 1; j++) {
-			int up = values[i-1][j] - 4;
-			int left = values[i][j-1] - 4;
-			int diagonal = values[i-1][j-1] - 1;
-			
-			if (first[i-1] == second[j-1]) {
-				diagonal += 3;
-			}
-			if (up > left && up > diagonal) {
-				values[i][j] = up;
-				Position prev(i-1, j);
-				positions[i][j] = prev;
-				if (up > maxValue) {
-					maxValue = up;
-					maxPosition = prev;
-				}
-			} else if (left > up && left > diagonal) {
-				values[i][j] = left;
-				Position prev(i, j-1);
-				positions[i][j] = prev;
-				if (left > maxValue) {
-					maxValue = left;
-					maxPosition = prev;
-				}
-			} else {
+    for (std::size_t j = 0; j < second.size() + 1; ++j)
+    {
+      row.push_back(0);
+      Position pos(0, 0);
+      positionRow.push_back(pos);
+    }
 
-				values[i][j] = diagonal;
-				Position prev(i-1, j-1); 
-				positions[i][j] = prev; 
-				if (diagonal > maxValue) {
-					maxValue = diagonal;
-					maxPosition = prev;
-				}
-			}
-		}
-	}
- 
+    values.push_back(row);
+    positions.push_back(positionRow);
+  }
 
-	std::vector<Position> path;
+  int maxValue = 0;
+  Position maxPosition(0, 0);
 
-	Position current = maxPosition;
-	while (true){
-		if (values[current.get(0)][current.get(1)] == 0) {
-			break;
-		}
+  for (std::size_t  i = 1; i < first.size() + 1; ++i)
+  {
+    for (std::size_t  j = 1; j < first.size() + 1; ++j)
+    {
+      int up = values[i-1][j] - 4;
+      int left = values[i][j-1] - 4;
+      int diagonal = values[i-1][j-1] - 1;
 
-		path.push_back(current);
-		current = positions[current.get(0)][current.get(1)];
-	}
+      if (first[i-1] == second[j-1])
+        diagonal += 3;
 
-	return path;
+      if (up > left && up > diagonal)
+      {
+        values[i][j] = up;
+        Position prev(i-1, j);
+        positions[i][j] = prev;
 
+        if (up > maxValue)
+        {
+          maxValue = up;
+          maxPosition = prev;
+        }
+      }
+      else if (left > up && left > diagonal)
+      {
+        values[i][j] = left;
+        Position prev(i, j-1);
+        positions[i][j] = prev;
+
+        if (left > maxValue)
+        {
+          maxValue = left;
+          maxPosition = prev;
+        }
+      }
+      else
+      {
+        values[i][j] = diagonal;
+        Position prev(i-1, j-1);
+        positions[i][j] = prev;
+
+        if (diagonal > maxValue)
+        {
+          maxValue = diagonal;
+          maxPosition = prev;
+        }
+      }
+    }
+  }
+
+  std::vector<Position> path;
+
+  Position current = maxPosition;
+  while (true)
+  {
+    if (values[current.get(0)][current.get(1)] == 0)
+      break;
+
+    path.push_back(current);
+    current = positions[current.get(0)][current.get(1)];
+  }
+
+  return path;
 }
 
 }
--- src/comparator.hpp
+++ src/comparator.hpp
@@ -8,20 +8,20 @@ namespace OLC
 
 // Helper class representing an (i, j) coordinate in the matrix formed by carthesian product of two vectors x and y.
 // Decodes to x[i] and y[j] to enable backtracking to find the longest overlap of the vectors.
-class Position {
-	private:
-		int i_, j_;
-	public:
-		Position(const int i, const int j);
-		int get(int index) const;	
+class Position
+{
+  private:
+    int i_, j_;
+  public:
+    Position(const int i, const int j);
+    int get(int index) const;
 };
 
 // Compares two vectors to find the longest possible overlap using the Smith-Waterman algorithm with a bit of a 
 // bigger price for insertions and removals to allow for more strict overlaps while maintaining the possibility
 // to handle large error reads.
-std::vector<Position> compare(std::vector<int> first, std::vector<int> second);
+std::vector<Position> compare(const std::vector<int>& first, const std::vector<int>& second);
 
 }
-
 
 #endif // COMPARATOR_HPP_
