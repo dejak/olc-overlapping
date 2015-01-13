bdc2347983a77b4feb0d72f20f3239feb4d14dc4
Josko <josko.nikolic1@gmail.com>
2015-01-13 08:14:14 +0100

    main: cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,6 @@ static std::queue<std::tuple<uint32_t, uint32_t>> g_tasks;
 
 static void worker()
 {
-  const std::thread::id this_id = std::this_thread::get_id();
-
   return;
 }
 
@@ -42,36 +40,28 @@ int main(int argc, char** argv)
 
   // read phase
   OLC::InputFileReader reader(file);
-  std::vector<OLC::Sequence*> sequences = reader.readSequences();
+  const std::vector<OLC::Sequence*> sequences = reader.readSequences();
 
   for (size_t i = 0; i < sequences.size(); ++i)
   {
-    auto sequence = sequences[i]->getNucleotides()->getSequence();
+    const auto sequence = sequences[i]->getNucleotides()->getSequence();
 
     // calculate minimizers - both interior and end minimizers
-    auto minimizers = minimize(sequence, w, k);
+    const auto minimizers = minimize(sequence, w, k);
   }
 
-  // generate basically tasks so we can do this in parallel if possible
+  // generate tasks so we can do this in parallel if possible
   std::queue<std::tuple<uint32_t, uint32_t>> tasks;
 
   for (uint32_t i = 0; i < sequences.size(); ++i)
-  {
     for (uint32_t j = i + 1; j < sequences.size(); ++j)
-    {
-      std::cout << "job (" << i << "," << j << ")\n";
       g_tasks.emplace(i, j);
-    }
-  }
 
   // use concurrent minimizer matching
   std::vector<std::thread> threads(std::thread::hardware_concurrency());
 
   for (uint8_t i = 0; i < threads.size(); ++i)
-  {
-    std::cout << "Thread [" << static_cast<int>(i + 1) << "] spawned\n";
     threads[i] = std::thread(worker);
-  }
 
   for (uint8_t i = 0; i < threads.size(); ++i)
     threads[i].join();
