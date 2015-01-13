fc8a00236e9f2748cbd34644a66f23bb565b2739
Josko <josko.nikolic1@gmail.com>
2015-01-13 15:01:53 +0100

    overlap: formatting

--- src/overlap.cpp
+++ src/overlap.cpp
@@ -2,24 +2,34 @@
 
 namespace OLC
 {
+
 Overlap::Overlap(const int startFirst, const int endFirst, const int startSecond, const int endSecond)
-  : startFirst_(startFirst), endFirst_(endFirst), startSecond_(startSecond), endSecond_(endSecond)
-  {}
+  : startFirst_(startFirst),
+    endFirst_(endFirst),
+    startSecond_(startSecond),
+    endSecond_(endSecond)
+{
 
-int Overlap::getStartFirst() const {
-	return startFirst_;
 }
 
-int Overlap::getEndFirst() const {
-	return endFirst_;
+int Overlap::getStartFirst() const
+{
+  return startFirst_;
 }
 
-int Overlap::getStartSecond() const {
-	return startSecond_;
+int Overlap::getEndFirst() const
+{
+  return endFirst_;
 }
 
-int Overlap::getEndSecond() const {
-	return endSecond_;
+int Overlap::getStartSecond() const
+{
+  return startSecond_;
+}
+
+int Overlap::getEndSecond() const
+{
+  return endSecond_;
 }
 
 }
--- src/overlap.hpp
+++ src/overlap.hpp
@@ -3,20 +3,23 @@
 
 namespace OLC
 {
+
 class Overlap
 {
   private:
-  	int startFirst_;
-  	int endFirst_;
-  	int startSecond_;
-  	int endSecond_;
+    int startFirst_;
+    int endFirst_;
+    int startSecond_;
+    int endSecond_;
+
    public:
-   	Overlap(const int startFirst, const int endFirst, const int startSecond, const int endSecond);
-   	int getStartFirst() const;
-   	int getEndFirst() const;
-   	int getStartSecond() const;
-   	int getEndSecond() const;
+    Overlap(const int startFirst, const int endFirst, const int startSecond, const int endSecond);
+    int getStartFirst() const;
+    int getEndFirst() const;
+    int getStartSecond() const;
+    int getEndSecond() const;
 };
+
 }
 
-#endif
+#endif // OVERLAP_HPP_
