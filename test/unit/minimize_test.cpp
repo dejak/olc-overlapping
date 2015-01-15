965e4bdae85aa0eb4c14049bd195789de351a3f3
Josko <josko.nikolic1@gmail.com>
2015-01-15 01:28:48 +0100

    minimize: fix test

--- test/unit/minimize_test.cpp
+++ test/unit/minimize_test.cpp
@@ -27,11 +27,11 @@ TEST_CASE("minimize", "[minimize]")
   vec.push_back(Nucleotide(NucleotideLetter(0x00)));
   vec.push_back(Nucleotide(NucleotideLetter(0x01)));
 
-  auto minimizers = minimize(vec, 3, 3);
+  auto* minimizers = minimize(vec, 3, 3);
 
   SECTION("test minimizer size")
   {
-    REQUIRE(minimizers.size() == 7);
+    REQUIRE(minimizers->size() == 7);
   }
 
   SECTION("test minimizer values")
@@ -44,12 +44,12 @@ TEST_CASE("minimize", "[minimize]")
     Minimizer minimizer5(vector<Nucleotide>{NucleotideLetter(0x02), NucleotideLetter(0x03), NucleotideLetter(0x01)}, 0);
     Minimizer minimizer6(vector<Nucleotide>{NucleotideLetter(0x02), NucleotideLetter(0x03), NucleotideLetter(0x03)}, 0);
 
-    REQUIRE(minimizers.at(0) == minimizer0);
-    REQUIRE(minimizers.at(1) == minimizer1);
-    REQUIRE(minimizers.at(2) == minimizer2);
-    REQUIRE(minimizers.at(3) == minimizer3);
-    REQUIRE(minimizers.at(4) == minimizer4);
-    REQUIRE(minimizers.at(5) == minimizer5);
-    REQUIRE(minimizers.at(6) == minimizer6);
+    REQUIRE(minimizers->at(0) == minimizer0);
+    REQUIRE(minimizers->at(1) == minimizer1);
+    REQUIRE(minimizers->at(2) == minimizer2);
+    REQUIRE(minimizers->at(3) == minimizer3);
+    REQUIRE(minimizers->at(4) == minimizer4);
+    REQUIRE(minimizers->at(5) == minimizer5);
+    REQUIRE(minimizers->at(6) == minimizer6);
   }
 }
