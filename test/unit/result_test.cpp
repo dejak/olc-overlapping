3eb183bf775df4b226df7797dad4697964847edd
Josko <josko.nikolic1@gmail.com>
2015-06-30 05:29:53 +0200

    result_test: fix spaces

--- test/unit/result_test.cpp
+++ test/unit/result_test.cpp
@@ -44,37 +44,37 @@ TEST_CASE("result class", "[result]")
 
   if (vec1.size() > overlapSecondEnd)
     bhang *= -1;
-	
+
   Result result(1, 2, overlapLength, ahang, bhang);
 
 
   SECTION("check length")
   {
-  	REQUIRE(result.getLength() == 3);
+    REQUIRE(result.getLength() == 3);
   }
 
   SECTION("check ahang and bhang") 
   {
-  	REQUIRE(result.getAhng() == 1);
-  	REQUIRE(result.getBhng() == -2);
+    REQUIRE(result.getAhng() == 1);
+    REQUIRE(result.getBhng() == -2);
   }
 
   const Overlap overlapReverse = compare(vec2, vec1);
   int32_t ahangReverse = overlapReverse.getStartFirst();
   int32_t bhangReverse = vec1.size() - overlapReverse.getEndSecond() - 1;
-  
+
   if (overlapReverse.getStartSecond() < overlapReverse.getStartFirst()) {
-  	ahangReverse *= -1;
+    ahangReverse *= -1;
   }
 
   if (vec2.size() > overlapReverse.getEndSecond()) {
-  	bhangReverse *= -1;
+    bhangReverse *= -1;
   }
 
   SECTION("check ahang and bhang in reverse case") 
   {
-  	REQUIRE(ahangReverse == -2);
-  	REQUIRE(bhangReverse == -2);
+    REQUIRE(ahangReverse == -2);
+    REQUIRE(bhangReverse == -2);
   }
 
   vector<Nucleotide> vec3;
@@ -95,17 +95,17 @@ TEST_CASE("result class", "[result]")
   int32_t bhangEnd = vec4.size() - overlapEnd.getEndSecond() -1;
 
   if (overlapEnd.getStartSecond() < overlapEnd.getStartFirst()) {
-  	ahangEnd *= -1;
+    ahangEnd *= -1;
   }
 
   if (vec4.size() > overlapEnd.getEndSecond()) {
-  	bhangEnd *= -1;
+    bhangEnd *= -1;
   }
 
   SECTION("check possible negative ahang/bhang in a trivial case")
   {
-  	REQUIRE(ahangEnd == 1);
-  	REQUIRE(bhangEnd == -1);
+    REQUIRE(ahangEnd == 1);
+    REQUIRE(bhangEnd == -1);
   }
 
 }
