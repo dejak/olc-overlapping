dbb03c5adaed277dc57aadc6a407a9f6f438b5f9
Josko <dinamo.forever@gmail.com>
2017-04-27 01:50:31 +0200

    Overlap: Header-only class to avoid ODR issues.

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ endif
 
 CXXFLAGS += $(OFLAGS) $(DFLAGS) -I.
 
-OBJS = main.o minimizer.o minimize.o nucleotide.o nucleotides.o fastq_parser.o fasta_parser.o sequence.o format_parser.o input_file_reader.o overlap.o
+OBJS = main.o minimizer.o minimize.o nucleotide.o nucleotides.o fastq_parser.o fasta_parser.o sequence.o format_parser.o input_file_reader.o
 PROGS = olc-overlapping
 
 all: $(OBJS) $(PROGS)
--- src/overlap.hpp
+++ src/overlap.hpp
@@ -7,19 +7,44 @@ namespace OLC
 class Overlap
 {
   private:
-    int startFirst_;
-    int endFirst_;
-    int startSecond_;
-    int endSecond_;
+    int32_t startFirst_;
+    int32_t endFirst_;
+    int32_t startSecond_;
+    int32_t endSecond_;
 
    public:
-    Overlap(const int startFirst, const int endFirst, const int startSecond, const int endSecond);
-    int getStartFirst() const;
-    int getEndFirst() const;
-    int getStartSecond() const;
-    int getEndSecond() const;
+    Overlap(const int32_t startFirst, const int32_t endFirst, const int32_t startSecond, const int32_t endSecond) :
+        startFirst_(startFirst),
+        endFirst_(endFirst),
+        startSecond_(startSecond),
+        endSecond_(endSecond)
+    {
+
+    }
+
+    inline int32_t getStartFirst() const
+    {
+      return startFirst_;
+    }
+
+    inline int32_t getEndFirst() const
+    {
+      return endFirst_;
+    }
+
+    inline int32_t getStartSecond() const
+    {
+      return startSecond_;
+    }
+
+    inline int32_t getEndSecond() const
+    {
+      return endSecond_;
+    }
+
 };
 
 }
+
 
 #endif // OVERLAP_HPP_
