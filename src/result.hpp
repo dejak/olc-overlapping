b3c1fd84d64974d0063b42246f0fa4cda4f76e0c
Josko <dinamo.forever@gmail.com>
2017-04-27 01:33:24 +0200

    Result: Heady-only clas to avoid ODR breakage.

--- src/result.hpp
+++ src/result.hpp
@@ -9,7 +9,7 @@
 namespace OLC
 {
 
-class Result
+class Result final
 {
   private:
     uint32_t identifier1_;
@@ -19,11 +19,34 @@ class Result
     int32_t bhang_;
 
   public:
-    Result(const uint32_t identifier1, const uint32_t identifier2, const uint32_t length, const int32_t ahang, const int32_t bhang);
-    std::tuple<uint32_t, uint32_t> getIdentifiers();
-    int32_t getAhng() const { return ahang_; }
-    int32_t getBhng() const { return bhang_; }
-    uint32_t getLength() const { return length_; }
+    Result(const uint32_t identifier1, const uint32_t identifier2, const uint32_t length, const int32_t ahang, const int32_t bhang) :
+      identifier1_(identifier1),
+      identifier2_(identifier2),
+      length_(length),
+      ahang_(ahang),
+      bhang_(bhang)
+    {
+    }
+
+    inline std::tuple<uint32_t, uint32_t> getIdentifiers() const
+    {
+      return std::make_tuple(identifier1_, identifier2_);
+    }
+
+    inline int32_t getAhng() const
+    {
+      return ahang_;
+    }
+
+    inline int32_t getBhng() const
+    {
+      return bhang_;
+    }
+
+    inline uint32_t getLength() const
+    {
+      return length_;
+    }
 };
 
 }
